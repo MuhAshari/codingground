12e311827a8784f333f6f56283e3ef74e9b702e4
Muhammad Ashari <31468210+MuhAshari@users.noreply.github.com>
2017-08-30 14:46:29 +0700

    Created by Coding Ground Client

---
+++ New Project-20170830/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello BRO / SIST");
+        System.out.println("NAMA : M.ASHARI");
+        System.out.println("NIM : 17220012");
+     }
+}
